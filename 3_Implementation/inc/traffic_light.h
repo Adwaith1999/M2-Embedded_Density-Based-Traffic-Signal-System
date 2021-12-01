e8ee829e88b1c8029bb56e3289512611bf9bdc5a
Adwaith1999 <94242252+Adwaith1999@users.noreply.github.com>
2021-12-01 21:28:27 +0530

    Update traffic_light.h

--- 3_Implementation/inc/traffic_light.h
+++ 3_Implementation/inc/traffic_light.h
@@ -1,1 +1,39 @@
+#ifndef __TRAFFIC_LIGHT_H_
+#define __TRAFFIC_LIGHT_H_
+/**
+ * @file traffic_light.h
+ * @author Adwaith.R
+ * @brief Project to Blink 12 LED connected to AVR328 MCU GPIO PIN
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+/**
+ * Macro Definitions
+ */
+
+#define R1 PB0 
+#define Y1 PB1
+#define G1 PB2
+
+#define R2 PB3
+#define Y2 PB4
+#define G2 PB5
+
+#define R3 PD5
+#define Y3 PD4
+#define G3 PD3
+
+#define R4 PD2
+#define Y4 PD1
+#define G4 PD0
+/**
+ * Include files
+ */ 
+#include "project_config.h"
+
+
+#endif /** __TRAFFIC_LIGHT_H_*/
